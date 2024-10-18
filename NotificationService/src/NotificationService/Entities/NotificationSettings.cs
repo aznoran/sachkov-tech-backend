f80a6dbc731ec706ca36985e6e1b15f7414d9a07
brinersam <vladbudyachenko@mail.ru>
2024-10-18 18:58:04 +0300

    Add model method that allows to update notification method preferences

--- NotificationService/src/NotificationService/Entities/NotificationSettings.cs
+++ NotificationService/src/NotificationService/Entities/NotificationSettings.cs
@@ -6,9 +6,20 @@ public class NotificationSettings
 
     public Guid UserId { get; init; }
 
-    public bool Email { get; init; } = true;
+    public bool Email { get; private set; } = true;
 
-    public bool Telegram { get; init; }
+    public bool Telegram { get; private set; }
 
-    public bool Web { get; init; } = true;
+    public bool Web { get; private set; } = true;
+
+    public void UpdatePreferences(
+        bool? email = null,
+        bool? telegram = null,
+        bool? web = null)
+    {
+        Email = email ?? Email;
+        Telegram = telegram ?? Telegram;
+        Web = web ?? Web;
+    }
+
 }
