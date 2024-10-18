f7a9b0b735d0e8f365553bbbabf614d9215b794d
brinersam <vladbudyachenko@mail.ru>
2024-10-18 18:59:31 +0300

    Add notification settings query functionality

---
+++ NotificationService/src/NotificationService/Features/Queries/GetNotificationSettings/GetNotificationSettingsHandler.cs
@@ -0,0 +1,32 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.EntityFrameworkCore;
+using NotificationService.Entities;
+using NotificationService.HelperClasses;
+using NotificationService.Infrastructure;
+
+namespace NotificationService.Features.Queries
+{
+    public class GetNotificationSettingsHandler
+    {
+        private readonly DbSet<NotificationSettings> _notificationSettings;
+
+        public GetNotificationSettingsHandler(ApplicationDbContext dbContext)
+        {
+            _notificationSettings = dbContext.Set<NotificationSettings>();
+        }
+
+        public async Task<Result<NotificationSettings, Error>> Handle(
+            GetNotificationSettingsQuery query,
+            CancellationToken cancellationToken = default)
+        {
+            var userSettings = await _notificationSettings.FirstOrDefaultAsync(
+                                        x => x.Id == query.Id,
+                                        cancellationToken);
+
+            if (userSettings == null)
+                return Error.NotFound("notification.settings.not.found", $"No settings were found with id: {query.Id}");
+
+            return userSettings;
+        }
+    }
+}
