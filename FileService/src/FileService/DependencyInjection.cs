9298fffde1973e6f8876dbbc0b93094c62c9077b
Perbudag <gasanalievp@gmail.com>
2024-10-19 12:59:11 +0300

    Создание прокта FileService.

---
+++ FileService/src/FileService/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿namespace FileService
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services, ConfigurationManager configurations)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddInfrastrucure(this IServiceCollection services, ConfigurationManager configurations)
+        {
+            return services;
+        }
+    }
+}
