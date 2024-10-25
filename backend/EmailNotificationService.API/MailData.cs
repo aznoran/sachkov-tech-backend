f08eb5156c4c92f0b6800a791acb5209b3a83a19
Nepoladki <79629242222@ya.ru>
2024-10-25 17:57:40 +0300

    Started EmailNotificatoinService

---
+++ backend/EmailNotificationService.API/MailData.cs
@@ -0,0 +1,26 @@
+﻿namespace EmailNotificationService.API
+{
+    public class MailData
+    {
+        // Reciever
+        public List<string> To { get; init; } = [];
+
+        // Content
+        public string Subject { get; init; } = string.Empty;
+        public string Body { get; init; } = string.Empty;
+
+        public MailData(
+                IEnumerable<string> to,
+                string subject,
+                string body)
+        {
+            // Reciever
+            To = to.ToList();
+
+            // Content
+            Subject = subject;
+            Body = body;
+        }
+
+    }
+}
