d4d3a64583ddbc3a065763d767a9725509d33664
brinersam <vladbudyachenko@mail.ru>
2024-11-05 19:56:25 +0300

    Add grpc notification service controller for testing

---
+++ backend/src/Issues/SachkovTech.Issues.Application/Features/GrpcNotificationServiceTester/Commands/AddNotificationSettings/AddNotificationSettingsCommand.cs
@@ -0,0 +1,8 @@
+﻿using SachkovTech.Core.Abstractions;
+
+namespace SachkovTech.Issues.Application.Features.GrpcNotificationServiceTester.Commands.AddNotificationSettings;
+
+public record AddNotificationSettingsCommand(
+    Guid UserId,
+    string Email,
+    string? WebEndpoint) : ICommand;
---
+++ backend/src/Issues/SachkovTech.Issues.Application/Features/GrpcNotificationServiceTester/Commands/PushNotification/PushNotificationCommand.cs
@@ -0,0 +1,11 @@
+﻿using SachkovTech.Core.Abstractions;
+using SachkovTech.Issues.Contracts.Dtos;
+
+namespace SachkovTech.Issues.Application.Features.GrpcNotificationServiceTester.Commands.PushNotification;
+
+public record PushNotificationCommand(
+    MessageDTO Message,
+    Guid[] UserIds,
+    Guid[] RoleIds,
+    string Type,
+    string Data) : ICommand;
