675786830775c53d1068073868f3715a7e295506
Alexandr <bagnencko.sasha1@gmail.com>
2024-11-12 14:01:52 +0300

    feat(migrations): added automatic context migration

---
+++ backend/src/Shared/SachkovTech.Core/Extensions/MigratorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SachkovTech.Core.Abstractions;
+
+namespace SachkovTech.Core.Extensions;
+
+public static class MigratorExtensions
+{
+    public static void RunMigrations(this IServiceProvider serviceProvider)
+    {
+        var migrators = serviceProvider.GetServices<IMigrator>();
+        foreach (var migrator in migrators)
+        {
+            migrator.Migrate();
+        }
+    }
+}
