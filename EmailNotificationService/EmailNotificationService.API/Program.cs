e923f944d630e27861f877cedb8c5aa1e37eed72
Ilya <79629242222@ya.ru>
2024-11-14 21:57:58 +0300

    seq

--- EmailNotificationService/EmailNotificationService.API/Program.cs
+++ EmailNotificationService/EmailNotificationService.API/Program.cs
@@ -22,15 +22,15 @@ services.AddScoped<EmailValidator>();
 services.AddScoped<MailSender>();
 services.AddScoped<MailConfirmationService>();
 
-//Log.Logger = new LoggerConfiguration()
-//           .WriteTo.Console()
-//           .WriteTo.Debug()
-//           .WriteTo.Seq(config.GetConnectionString("Seq")
-//               ?? throw new ArgumentNullException("Seq connection string was not found"))
-//           .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Information)
-//           .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Information)
-//           .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Information)
-//           .CreateLogger();
+Log.Logger = new LoggerConfiguration()
+           .WriteTo.Console()
+           .WriteTo.Debug()
+           .WriteTo.Seq(config.GetConnectionString("Seq")
+               ?? throw new ArgumentNullException("Seq connection string was not found"))
+           .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Information)
+           .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Information)
+           .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Information)
+           .CreateLogger();
 
 services.AddSerilog();
 
