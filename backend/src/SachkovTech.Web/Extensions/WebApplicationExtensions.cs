c406ad5b8e55ca55e98c7cb597656ec0b9febd83
Alexandr <bagnencko.sasha1@gmail.com>
2024-11-27 12:52:26 +0300

    refactor(program.cs): refactor and add auto migration

---
+++ backend/src/SachkovTech.Web/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using SachkovTech.Core.Extensions;
+using SachkovTech.Framework.Middlewares;
+using Serilog;
+
+namespace SachkovTech.Web.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static void Configure(this WebApplication app)
+    {
+        app.Services.RunMigrations();
+
+        app.UseExceptionMiddleware();
+        app.UseSerilogRequestLogging();
+        app.ConfigureCors();
+        app.UseAuthentication();
+        app.UseScopeDataMiddleware();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+    private static void ConfigureCors(this WebApplication app)
+    {
+        app.UseCors(config =>
+        {
+            config.WithOrigins("http://localhost:5173")
+                .AllowCredentials()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+    }
+}
