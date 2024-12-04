cbe435bbd338539b0e7a245c424e626f4b4df522
kirill_sachkov <sachkovkrl@gmail.com>
2024-12-04 14:59:38 +0300

    fix auth

--- backend/src/Shared/SachkovTech.Framework/Authorization/PermissionRequirementHandler.cs
+++ backend/src/Shared/SachkovTech.Framework/Authorization/PermissionRequirementHandler.cs
@@ -21,12 +21,16 @@ public class PermissionRequirementHandler : AuthorizationHandler<PermissionAttri
         if (context.User.Identity is null || !context.User.Identity.IsAuthenticated
                                           || _httpContextAccessor.HttpContext is null)
         {
+            context.Fail();
             return Task.CompletedTask;
         }
 
         var userScopedData = _httpContextAccessor.HttpContext.RequestServices.GetRequiredService<UserScopedData>();
         if (!userScopedData.Permissions.Contains(permission.Code))
+        {
+            context.Fail();
             return Task.CompletedTask;
+        }
 
         context.Succeed(permission);
         return Task.CompletedTask;
